b5f7f3b7c73d95538bf51c3199f4bcfb91e48b37
criquet1 <criquet1@oricom.ca>
2026-06-28 12:56:55 -0400

    Ajout login/logout et protection des routes

--- facture/urls.py
+++ facture/urls.py
@@ -1,15 +1,19 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='accueil'),
-    path('facture/', views.facture, name='facture'),
-    path('releve-bancaire/', views.releve_bancaire, name='releve_bancaire'),
-    path('journal-general/', views.journal_general, name='journal_general'),
-    path('grand-livre/', views.grand_livre, name='grand_livre'),
-    path('balance-de-verification/', views.balance_de_verification, name='balance_de_verification'),
-    path('compte-a-payer/', views.compte_a_payer, name='compte_a_payer'),
-    path('compte-a-recevoir/', views.compte_a_recevoir, name='compte_a_recevoir'),
-    path('rapport-de-taxes/', views.rapport_de_taxes, name='rapport_de_taxes'),
+    path('login/', LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user=True), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', login_required(views.index), name='accueil'),
+    path('facture/', login_required(views.facture), name='facture'),
+    path('releve-bancaire/', login_required(views.releve_bancaire), name='releve_bancaire'),
+    path('journal-general/', login_required(views.journal_general), name='journal_general'),
+    path('grand-livre/', login_required(views.grand_livre), name='grand_livre'),
+    path('balance-de-verification/', login_required(views.balance_de_verification), name='balance_de_verification'),
+    path('compte-a-payer/', login_required(views.compte_a_payer), name='compte_a_payer'),
+    path('compte-a-recevoir/', login_required(views.compte_a_recevoir), name='compte_a_recevoir'),
+    path('rapport-de-taxes/', login_required(views.rapport_de_taxes), name='rapport_de_taxes'),
 ]
--- lecarnet/settings.py
+++ lecarnet/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'accueil'
+LOGOUT_REDIRECT_URL = 'login'
